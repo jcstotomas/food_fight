f696fe3da96494f7e97465b4562ada4b776c9523
jcstotomas <jcstotomas@yahoo.com>
2020-06-16 19:49:10 -0700

    added restaurant class

--- food_fight_backend/app/api/restaurant.py
+++ food_fight_backend/app/api/restaurant.py
@@ -1,10 +1,11 @@
 from app.api import bp
 import os
 import requests
+from app.classes import Restaurant
 api_key = os.getenv("API_KEY")
 API_ENDPOINT = "https://api.yelp.com/v3/businesses/search"
 @bp.route("restaurants", methods=["GET"])
-def get_random(location="irvine", limit=5):
+def get_random_restaurant(location="irvine", limit=5):
     payload = {
         "location": location,
         "limit": 5
@@ -14,3 +15,10 @@ def get_random(location="irvine", limit=5):
     }
     data = requests.get(API_ENDPOINT, params=payload, headers=headers)
     return data.json()
+
+
+
+def ingest_data(data_json):
+    # restaurant name, cuisine, image. hyperlink to the yelp page 
+    
+    pass
